64b73412ea92e26dd23f1e20bc7649819b68fe1b
qievenz <ivann96@gmail.com>
2021-06-12 17:27:28 -0300

    object lock

---
+++ 02.DataSharingAndSynchronization/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _02.DataSharingAndSynchronization
+{
+    public class BankAccount
+    {
+        public object padlock = new object();
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            lock (padlock)
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            lock (padlock)
+            {
+                Balance -= amount;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount();
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        ba.Deposit(100);
+                    }
+                }));
+
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        ba.Withdraw(100);
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine(ba.Balance);
+        }
+    }
+}
